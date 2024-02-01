c7c77a2392ec37d68119d32a0d45835b11d6fb24
Aman-1610 <111713832+Aman-1610@users.noreply.github.com>
2024-02-01 06:08:54 +0530

    Add files via upload

---
+++ ArithmeticOperator.java
@@ -0,0 +1,12 @@
+class ArithmeticOperator
+{
+public static void main(String args[])
+{
+int num1=10,num2=2;
+System.out.println(num1+num2);
+System.out.println(num1-num2);
+System.out.println(num1*num2);
+System.out.println(num1/num2);
+System.out.println(num1%num2);
+}
+}
---
+++ Datatypes.java
@@ -0,0 +1,21 @@
+class Datatypes
+{
+   public static void main(String args[])
+     {
+        char a = 'A';
+        int b = 788;
+        byte c = 9;
+        short d=38;
+        double e=8.987654321;
+        float f=9.8888888f;
+        long g=1228938839;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+      }
+}
