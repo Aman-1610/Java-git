0877b5be02bbf48e0f3aea797e1bf788c514638f
Aman kumar <amandiv2345@gmail.com>
2024-02-05 06:38:22 +0530

    Practise questions of java

---
+++ HelloHiAgain.java
@@ -0,0 +1,10 @@
+/*Write a program to print two message in two lines using one print function 
+and you don't have to use println function*/
+
+class HelloHiAgain
+{
+  public static void main(String args[])
+{
+  System.out.print("Hello java! \n"+"Hi");
+}
+}
